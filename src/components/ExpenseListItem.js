8b8cb2f0885c0efd370d5dcfb99f0ad5d30ae1f6
Obidi Isaac <obidi.isaacw@gmail.com>
2018-10-08 15:58:48 +0100

    Formated amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,14 +2,24 @@ import React from  'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import  removeExpence  from '../AppStateManagement/actions/removeExpence'
+import moment from 'moment'
+import numeral from 'numeral'
+
+
+
+
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
   <Link to={`/edit/${id}`}><h6>{description}</h6></Link>
-  <b>{amount} - {createdAt}</b>
+  <b>
+      {numeral(amount / 100).format('$0,0.00')}
+        -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+  </b>
   <button onClick={()=>{dispatch(removeExpence({id}))}}>remove</button>
-
   </div>
 )
 
+//₦
 export default connect()(ExpenseListItem)
